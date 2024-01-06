31123a8f7c1fc645791d7dce70d3606120f9c5de
tigranpetrosan <tapetrosian@gmail.com>
2024-01-06 21:14:52 +0200

    Tips are ready

---
+++ app/src/main/java/com/example/layoutsapp/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.layoutsapp
+
+import android.health.connect.datatypes.units.Length
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.layoutsapp.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.PI
+import kotlin.math.sqrt
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.optionTwentyPercent -> 0.20
+            R.id.optionEighteenPercent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage * cost
+        if (binding.roundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+}
